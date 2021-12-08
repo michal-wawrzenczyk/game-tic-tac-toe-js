0b739795cf85fe756d481c0f6f1dc9bb39473905
Michał Wawrzeńczyk <wawrzenczyk.m@yahoo.com>
2021-12-08 13:24:14 +0100

    feat(draw-and-restart): create message for draw instance, implement the restart button

--- script.js
+++ script.js
@@ -15,18 +15,30 @@ const winning_COMBINATIONS = [
 
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
+const winningMessageTextElement = document.querySelector('[data-message-text]');
+const winningMessageElement = document.getElementById('message');
+const restartButton = document.getElementById('restartButton');
 
 let oTurn // If the variable is true, then it's O's turn, if false, then it's X's turn.
 
 gameStart();
+restartButton.addEventListener('click', gameStart);
+// every time we click on it, we just want to call a gameStart function.
 
 function gameStart() {
     oTurn = false; // to start the game
     cellElements.forEach(cell => {
+        cell.classList.remove(x_CLASS);
+        cell.classList.remove(o_CLASS);
+        // to remove all marks from the board after click on the Restart button.
+        cell.removeEventListener('click', handleClick);
+        // to remove the event listener on the click together with the handleClick function.
         cell.addEventListener('click', handleClick, {once: true});
         // once we click on chosen cell, the event listener is not going to fire again when we click on the cell again. It only does a click event once in one cell.
     })
     setBoardHoverClass();
+    winningMessageElement.classList.remove('show');
+    // it will turn off the winning message window
 }
 
 function handleClick(e) {
@@ -37,12 +49,18 @@ function handleClick(e) {
     placeMark(cell, currentClass);
     // Check for Win
     if (checkWin(currentClass)) {
-        console.log('Winner!');
+       // console.log('Winner!');
+        gameEnd(false);
+        // is it a draw or maybe not?
+    } else if (isDraw()) {
+        gameEnd(true);
+        // Check for Draw
+    } else {
+        swapTurns();
+        // we want to swap turns, if there is no winner yet.
+        setBoardHoverClass();
+        // Switch Turns - if none of those above happen
     }
-    // Check for Draw
-    // Switch Turns - if none of those above happen
-    swapTurns();
-    setBoardHoverClass();
 }
 
 function placeMark(cell, currentClass) {
@@ -76,4 +94,24 @@ function checkWin(currentClass) {
         })
     })
     // it will return true, if any of the values inside it are true. Combination is going to loop over all the combinations. For each one it must check if all the values inside cell elements have the same class. If current class is placed in one of the all winning combinations - it wins. If every single cell inside the combination is correct for at least one of the winning combinations then it is a win.
+}
+
+function gameEnd(draw) {
+    if (draw) {
+        winningMessageTextElement.innerText = 'Draw!';
+    } else {
+        winningMessageTextElement.innerText = `${oTurn ? "O's" : "X's"} Wins!`;
+        // it's going to check if it's circles turn, then print out O's, if X's = print X's and then print out Wins.
+    }
+    winningMessageElement.classList.add('show');
+    // if someone wins the game, the text will be printed and it's going to show the winning message element.
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        // cellElements does not have an "every" method, so we must destructure it into an array => [...cellElements]
+        return cell.classList.contains(x_CLASS) || cell.classList.contains(o_CLASS);
+        // we want to check if every cell has a class x or o. If every class has a x or o class, then we want to return true, because it is a draw.
+    })
+    // it needs to check if every single cell has been filled.
 }
